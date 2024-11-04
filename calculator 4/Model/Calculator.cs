80e3eef7890afdb7ef137724e379de616ef33829
park <tkdwns1026@naver.com>
2024-11-04 14:28:47 +0900

    Calculator

---
+++ calculator 4/Model/Calculator.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator7
+{    
+    public class Calculator
+    {
+        public string Input = "";       // 숫자 입력  계산기
+        public string Operator = "";    // 연산자 입력
+        public string Result = "0";     // 계산기의 결과창
+        public double result = 0;       // 연산자 이전의 Input 값
+        public string Save = "";        // 계산기의 결과창 위쪽 식 
+        public string equation = "";
+        public double Perresult = 0;
+
+        public bool ResultTF = false;
+
+        public void Append(object number)
+        {
+            if (ResultTF == true || Result == "0")
+            {
+                Input = number.ToString();
+                Result = Input;
+                ResultTF = false;
+                //Save = "";
+            }        
+            
+            else
+            {
+                Input += number;
+                Result = Input;
+            }
+        }
+
+        public void Dot()
+        {
+            if (!Input.Contains("."))
+            {
+                if (Input == "")
+                {
+                    Append("0.");
+                }
+                else
+                    Append(".");
+
+            }
+        }
+
+        public string UseOperator(string operation)
+        {
+            string BeforeEq = string.Empty;
+            string BeforeRe = string.Empty;
+            double value;
+            if (double.TryParse(Input, out value))
+            {
+                // save의 가장 마지막이 =인지 확인                    
+                if (Save.EndsWith("="))
+                {
+                    Save = result.ToString();
+                }
+                /*if (ResultTF == true)
+                {
+                    result = value;
+                    Save += operation.ToString();
+                    ResultTF = false;
+                }*/
+                if (ResultTF == false)
+                {
+                    if (!string.IsNullOrEmpty(Operator))
+                    {
+                        BeforeEq = result + Operator + value + "=";
+                        result = OP(result, value, Operator);
+                        BeforeRe = result.ToString();
+                        Save = $"{result}{operation}";
+                        Result = result.ToString();
+                        
+                    }
+                    else
+                    {
+                        result = value;
+                        Save = $"{result}{operation}";
+                    }
+                }
+                else
+                {
+                    Input = Result;
+                    Operator = operation;
+                    Save = $"{result}{Operator}";
+                }
+            }
+            else if (!string.IsNullOrEmpty(operation))
+            {
+                Operator = operation;
+                Save = $"{result}{Operator}";
+            }
+            Operator = operation;
+            Input="";
+            return BeforeEq + BeforeRe;
+        }
+
+        public static double OP(double left, double right, string operation)
+        {
+            switch (operation)
+            {
+                case "+":
+                    return left + right;
+                case "-":
+                    return left - right;
+                case "*":
+                    return left * right;
+                case "/":
+                    return right != 0 ? left / right : double.NaN;
+                default:
+                    return double.NaN;
+
+            }
+        }
+
+        public string Equal()
+        {
+            if (!string.IsNullOrEmpty(Input))
+            {
+                double Value;
+                if (double.TryParse(Input, out Value))
+                {
+                    result = OP(result, Value, Operator);
+                    Save = $"{Save}{Input}=";
+                    Input = result.ToString();
+                    Result = Input;
+                    Operator = "";
+                    ResultTF = true;
+                    return Save + Result;
+
+                }
+            }
+            return "";
+        }
+
+        public void Back()
+        {
+            if (Input.Length > 0)
+            {
+                Input = Input.Substring(0, Input.Length - 1);
+                if (Input.Length > 0)
+                {
+                    Result = Input;
+                }
+                else
+                {
+                    Result = "0";
+                }
+            }
+        }
+
+        public void Clear()
+        {
+            Input = "";
+            Operator = "";
+            Result = "0";
+            result = 0;
+            Save = "";            
+            ResultTF = false;
+        }
+
+        public void ClearEntry()
+        {
+            Input = "";
+            Result = "0";
+            result = 0;
+            ResultTF = false;
+        }
+
+        public void Percent()
+        {
+            double value;
+            if (double.TryParse(Input, out value))
+            {
+                Perresult = value / 100;
+                Result = Perresult.ToString();
+                Input = Perresult.ToString();                         
+            }
+
+        }
+                           
+        /* public void Reverse()
+{
+   if (!string.IsNullOrEmpty(Input))
+   {
+       double value;
+       if (double.TryParse(Input, out value) && value != 0)
+       {
+           result = 1 / value;
+           Save = "1 /(" + value.ToString() + ")";
+           Input = result.ToString();
+           Result = Input;
+       }
+   }
+}
+
+public void Square()
+{
+   if (!string.IsNullOrEmpty(Input))
+   {
+       double value;
+       if (double.TryParse(Input, out value))
+       {
+           result = value * value;
+           Input = result.ToString();
+           Save = "sqr(" + value.ToString() + ")";
+           Result = Input;
+           ResultTF = true;
+       }
+   }
+}
+
+public void SquareRoot()
+{
+   if (!string.IsNullOrEmpty(Input))
+   {
+       double value;
+       if (double.TryParse(Input, out value) && value >= 0)
+       {
+           result = Math.Sqrt(value);
+           Input = result.ToString();
+           Save = "sqrt(" + value.ToString() + ")";
+           Result = Input;
+           ResultTF = true;
+       }
+   }
+}
+
+public void PlusMinus()
+{
+   if (!string.IsNullOrEmpty(Input))
+   {
+       double value;
+       if (double.TryParse(Input, out value))
+       {
+           result = -value;
+           Input = result.ToString();
+           Result = Input;
+       }
+   }
+}*/
+    }
+}
+    
+
+
+
+
---
+++ calculator 4/ViewModel/MainViewModel.cs
@@ -0,0 +1,222 @@
+﻿using Calculator7;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+
+
+
+namespace Calculator7.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public Calculator calc;
+
+        public string Result
+        {
+            get => calc.Result.ToString();
+            set
+            {
+                calc.Result = value;
+
+                OnPropertyChanged(nameof(Result));
+            }
+        }
+
+        public string Save
+        {
+            get => calc.Save;
+            set
+            {
+                OnPropertyChanged(nameof(Save));
+            }
+        }
+
+        public string Equation
+        {
+            get => calc.equation;
+            set
+            {
+                OnPropertyChanged(nameof(Equation));
+            }
+        }
+
+        public ObservableCollection<string> Equations { get; set; }
+        
+
+
+        public ICommand ClickCommand { get; set; }
+        public ICommand ClickOpCommand { get; set; }
+        public ICommand EqualCommand { get; set; }
+        public ICommand ClearCommand { get; set; }
+        public ICommand BackCommand { get; set; }
+        public ICommand DotCommand { get; set; }
+        public ICommand CECommand { get; set; }
+        public ICommand PERCommand { get; set; }
+        //public ICommand ReverseCommand { get; set; }
+        //public ICommand SqrCommand { get; set; }
+        //public ICommand SqrtCommand { get; set; }
+        //public ICommand PlusMinusCommand { get; set; }      
+
+        public ICommand CopyCommand { get; set; }
+        public ICommand PasteCommand { get; set; }
+
+        public IValueConverter Converter { get; set; }
+        public bool? IsChecked { get; set; }
+
+        public ICommand ExitCommand { get; set; }
+        
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        public MainViewModel()
+        {
+            calc = new Calculator();
+            Equations = new ObservableCollection<string>();
+            
+
+            ClickCommand = new RelayCommand(Btn_Click);
+            ClickOpCommand = new RelayCommand(BtnOp_Click);
+            EqualCommand = new RelayCommand(Equal_Click);
+            ClearCommand = new RelayCommand(Clear_Click);
+            BackCommand = new RelayCommand(Back_Click);
+            DotCommand = new RelayCommand(Dot_Click);
+            CECommand = new RelayCommand(CE_Click);
+            PERCommand = new RelayCommand(Per_Click);
+            //ReverseCommand = new RelayCommand(Reverse_Click);
+            //SqrCommand = new RelayCommand(Sqr_Click);
+            //SqrtCommand = new RelayCommand(Sqrt_Click);
+            //PlusMinusCommand = new RelayCommand(PlusMinus_Click);
+
+            CopyCommand = new RelayCommand(CopyText);
+            PasteCommand = new RelayCommand(PasteText);
+
+            ExitCommand = new RelayCommand(Exitbtn_Click);
+
+        }
+
+        public void CopyText(object obj)
+        {
+            if (Result.Length > 0)
+            {
+                Clipboard.SetText(Result);
+            }
+        }
+
+        public void PasteText(object obj)
+        {
+            if (Clipboard.ContainsText())
+            {
+                Result = Clipboard.GetText();
+                calc.Input = Result;
+                calc.ResultTF = false;
+            }
+        }
+
+        public void Exitbtn_Click(object parameter)
+        {            
+            Application.Current.Shutdown();
+        }
+
+        public void Btn_Click(object number)
+        {
+            string input = number.ToString();
+            calc.Append(input);
+            OnPropertyChanged(nameof(Result));
+        }
+
+        public void BtnOp_Click(object operation)
+        {
+            string op = operation.ToString();
+            string result = calc.UseOperator(op);
+            if (result != "")
+            {
+                Equations.Add(result);
+            }            
+            OnPropertyChanged(nameof(Result));
+            OnPropertyChanged(nameof(Save));
+        }
+
+        public void Equal_Click(object parameter)
+        {
+            Equations.Add(calc.Equal());
+            // calc.Save = Result;
+            // calc.Save = calc.Equal();
+            OnPropertyChanged(nameof(Result));
+            OnPropertyChanged(nameof(Save));
+        }
+
+        public void Clear_Click(object parameter)
+        {
+            calc.Clear();
+            Equations.Clear();
+            OnPropertyChanged(nameof(Result));
+            OnPropertyChanged(nameof(Save));
+        }
+
+        public void Back_Click(object parameter)
+        {
+            calc.Back();
+            OnPropertyChanged(nameof(Result));
+        }
+
+        public void Dot_Click(object parameter)
+        {
+            calc.Dot();
+            OnPropertyChanged(nameof(Result));
+        }
+
+        public void CE_Click(object parameter)
+        {
+            calc.ClearEntry();
+            OnPropertyChanged(nameof(Result));
+        }
+
+        public void Per_Click(object parameter)
+        {
+            calc.Percent();
+            OnPropertyChanged(nameof(Result));
+            OnPropertyChanged(nameof(Save));
+        }
+
+        /* public void Reverse_Click(object parameter)
+        {
+            calc.Reverse();
+            OnPropertyChanged(nameof(Result));
+            OnPropertyChanged(nameof(Save));
+        }
+
+        public void Sqr_Click(object parameter)
+        {
+            calc.Square();
+            OnPropertyChanged(nameof(Result));
+            OnPropertyChanged(nameof(Save));
+        }
+
+        public void Sqrt_Click(object parameter)
+        {
+            calc.SquareRoot();
+            OnPropertyChanged(nameof(Result));
+            OnPropertyChanged(nameof(Save));
+        }
+
+        public void PlusMinus_Click(object parameter)
+        {
+            calc.PlusMinus();
+            OnPropertyChanged(nameof(Result));
+        }
+        */
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }       
+    }
+}
